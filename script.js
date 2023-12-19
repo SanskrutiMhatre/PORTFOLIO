991bcedec8eebd2be970d067172c7cf405adddbb
SanskrutiMhatre <mhatresanskruti42@gmail.com>
2023-12-19 15:26:30 +0530

    new commit

--- script.js
+++ script.js
@@ -13,28 +13,3 @@ const typed = new Typed('.typed-text', {
     loop: true
   });
   
-
-  
-  document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission
-    
-    // Get form values
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    
-    // Push data to Firebase database
-    database.ref("contacts").push().set({
-      name: name,
-      email: email,
-      message: message
-    }).then(() => {
-      // Data has been sent to Firebase
-      console.log("Data sent to Firebase!");
-      // Optional: Show success message or redirect
-    }).catch(error => {
-      // Handle errors
-      console.error("Error sending data:", error);
-      // Optional: Show error message
-    });
-  });
